f62cf4bb463f576b239cf0b27684ee10c4e2d0a3
poojavalecha <117039510+poojavalecha@users.noreply.github.com>
2023-10-31 17:24:35 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Pooja <code>src/App.js</code> loves Atharva.
         </p>
         <a
           className="App-link"
